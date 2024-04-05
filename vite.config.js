524e1ad1063121005d90faf693aeaeb299edaa79
kiccer <1072907338@qq.com>
2024-04-05 19:32:16 +0800

    🐎 ci: 配置修改

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
+
     plugins: [
         vue(),
         vueJsx(),
