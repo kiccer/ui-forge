c2131bc8f96d9f0478de367508ef4f259a031205
kiccer <1072907338@qq.com>
2024-04-05 19:34:59 +0800

    🐎 ci: 改为 hash 路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { menuRoutes } from './menuRoute'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
 
     routes: [
         ...menuRoutes
